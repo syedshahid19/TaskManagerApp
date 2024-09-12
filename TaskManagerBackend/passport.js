11c6d99a626eacd2641faf752989fee7619263aa
syedshahid19 <syedshahid9550@gmail.com>
2024-09-13 01:13:44 +0530

    Changes Done after backend deployment

--- TaskManagerBackend/passport.js
+++ TaskManagerBackend/passport.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "https://taskmanagerapp-0lb7.onrender.com/auth/google/callback",
   passReqToCallback: true,
 }, 
 async (request, accessToken, refreshToken, profile, done) => {
