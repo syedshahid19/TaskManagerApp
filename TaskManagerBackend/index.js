6b7313330acd800cb98e8e90a6a0c4415c19cd34
syedshahid19 <syedshahid9550@gmail.com>
2024-09-13 01:29:23 +0530

    done

--- TaskManagerBackend/index.js
+++ TaskManagerBackend/index.js
@@ -13,6 +13,14 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
+
+
 app.use(
 	cors({
 		origin:"*",
